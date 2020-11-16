7e84307d5a6074e9eeee2bf8e12e4c682a94ff03
张文昊 <815636627@qq.com>
2020-11-16 23:28:02 +0800

    练习

---
+++ Pratice_20_11_16/test.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+#include<limits.h>
+#include<ctype.h>
+enum State
+{
+	VALID,
+	INVALID
+}; state = VALID;
+int my_atoi(const char * str)
+{
+	int flag = 0;
+	state = INVALID;
+	//空指针处理
+	if (str == NULL)
+		return 0;
+
+	//只有一个空字符的处理(一个空字符也是不能转换的）
+	if (*str == '\0')
+		return 0;
+
+	//空白字符的处理(不做处理，跳过空白字符）
+	while (isspace(*str))
+	{
+		str++;
+	}
+
+	//正负号处理
+	if (*str == '+')
+	{
+		flag = 1;
+		str++;
+	}
+	else if (*str == '-')
+	{
+		flag = -1;
+		str++;
+	}
+
+	//判断*str是否是数字字符
+	long long ret = 0;
+	while (isdigit(*str))//如果是数字字符就进来
+	{
+		ret = ret * 10 + flag*(*str - '0');
+		if (ret > INT_MAX || ret < INT_MIN)
+		{
+			ret = 0;
+			return (int)ret;
+		}
+		str++;
+	}
+	//当不是数字字符时，跳到这里
+	if (*str == '\0')
+	{
+		state = VALID;
+		return (int)ret;
+	}
+	return (int)ret;
+}
+int main()
+{
+	char * p = "-123";
+	int ret = my_atoi(p);
+	printf("%d\n", ret);
+	return 0;
+}
