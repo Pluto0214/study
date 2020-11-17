104f92ccdfe582398ec5b523c877a4192f413719
张文昊 <815636627@qq.com>
2020-11-17 23:27:39 +0800

    练习

---
+++ Pratice_20_11_17/test.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+unsigned char i = 0;
+int main()
+{
+	for (i = 0; i <= 255; i++)
+	{
+		printf("hello world\n");
+	}
+	return 0;
+}
+
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i<1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d", strlen(a));
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a = 128;
+//	printf("%u\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a = -128;
+//	printf("%u\n", a);
+//	return 0;
+//}
+//10000000000000000000000010000000
+//11111111111111111111111101111111
+//11111111111111111111111110000000
+//
+
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("a=%d,b=%d,c=%d", a, b, c);
+//	return 0;
+//}////输出结果-1 -1 255
+
+//判断大小端
+//int check_sys()
+//{
+//	return (*(char *)&a);
+//}
+
+//int check_sys()
+//{
+//	int a = 1;
+//	char * p = (char *)&a;
+//	if (*p == 1)
+//		return 1;
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int ret = check_sys();
+//	if (ret == 1)
+//	{
+//		printf("小端");
+//	}
+//	else
+//	{
+//		printf("大端");
+//	}
+//	return 0;
+//}
