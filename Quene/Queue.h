333f6db03af4b2c0bb3de34c4f34a0b0de8fdb47
张文昊 <815636627@qq.com>
2021-01-17 18:02:23 +0800

    队列

---
+++ Quene/Queue.h
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma once
+#include<stdio.h>
+#include<assert.h>
+typedef int QDataType;
+
+typedef struct QueueNode
+{
+	struct QueueNode* _next;
+	QDataType _data;
+}QueueNode;
+
+typedef struct Queue
+{
+	QueueNode* _head;
+	QueueNode* _tail;
+}Queue;
+
+void QueueInit(Queue* pq);
+void QueueDestory(Queue* pq);
+void QueuePush(Queue* pq, QDataType x);
+void QueuePop(Queue* pq);
+QDataType QueueFront(Queue* pq);
+QDataType QueueBack(Queue* pq);
+
+//返回1是空，返回0是非空
+int QueueEmpty(Queue* pq);
+int QueueSize(Queue* pq);
